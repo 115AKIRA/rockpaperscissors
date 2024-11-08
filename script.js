7d2791809a3e015622c57a3bf694b30a2f684c1e
Franck BAGGIO-KOST <franck.baggio-kost@mewo-campus.fr>
2024-11-08 15:19:33 +0100

    v1 done

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ window.addEventListener("load", () => {
 		["31", "12", "23"]  //p2 win
 	];
 	
+	const win_condition = [ 
+	//   tie   p2    p1
+		"11", "12", "13", 
+		"22", "23", "21", 
+		"33", "31", "32" 
+	] // mod of win_condition by 3 : tie if 0, p2 win if 1, p1 win if 2
+	
 	console.log(combinations);
 	
 	function startGame() {
@@ -45,20 +52,55 @@ window.addEventListener("load", () => {
 		
 			imagesjeux[0].src = (e.target.attributes.src.value);
 			
-			console.log(imagesjeux[0]);
+			score = 0;
 			
 			score += 10 + ( 10 * imageslist.indexOf(imagesjeux[0].attributes.src.value) );
-			console.log("score = " + score);
 			let rng = Math.floor(Math.random() * 3);
-			console.log(rng);
+			
+			score = score + 1 + ( 1 * rng );
+			
+			let win = ( win_condition.indexOf(score.toString()) % 3 );
+			console.log("score = " + score + " | win = " + win );
 			
 			setTimeout( () => {
-				score = score + 1 + ( 1 * rng );
-				console.log(score);
-				imagesjeux[1].src = imageslist[rng];
 				
-				console.log(score);
+				imagesjeux[1].src = imageslist[rng];	
+
+				if (win == 0) {
+					
+					imagesjeux[0].classList.add("tie");
+					imagesjeux[1].classList.add("tie");
+					
+				} else if (win == 1) {
+					
+					imagesjeux[0].classList.add("loser");
+					imagesjeux[1].classList.add("winner");
+					scores[1].innerText = ( parseInt(scores[1].innerText) + 1 );
+					
+				} else if (win == 2) {
+					
+					imagesjeux[0].classList.add("winner");
+					imagesjeux[1].classList.add("loser");
+					scores[0].innerText = ( parseInt(scores[0].innerText) + 1 );
+					
+				}
+				
+				setTimeout( () => {
+					
+					for ( let k = 0 ; k < imagesjeux.length ; k++ ) {
+						
+						imagesjeux[k].classList.remove("winner");
+						imagesjeux[k].classList.remove("loser");
+						imagesjeux[k].classList.remove("tie");
+						imagesjeux[k].src = "questionmark.png";
+						
+					}
+					
+				}, "1000");
+				
 			}, "1000");
+			
+			
 			
 		
 		});
